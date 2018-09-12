0a7485351fe416f332d21041b354c3c3b460d050
Exleryz <zyezhou@163.com>
2018-09-12 13:41:22 +0800

    spring配置修改 registerService创建

---
+++ movie-sso/movie-sso-service/src/main/java/com/byx/movie/sso/service/impl/RegisterServiceImpl.java
@@ -0,0 +1,15 @@
+package com.byx.movie.sso.service.impl;
+
+import com.byx.movie.sso.service.RegisterService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Exler
+ * Time 2018-09-12 13:32
+ * Description:
+ * @FileName RegisterServiceImpl
+ */
+@Service
+public class RegisterServiceImpl implements RegisterService {
+
+}
