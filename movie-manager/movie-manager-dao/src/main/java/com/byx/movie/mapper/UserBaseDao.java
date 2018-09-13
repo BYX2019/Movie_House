ddfab72679ac6ec2856dc4abacf1847fb00e9972
Exleryz <zyezhou@163.com>
2018-09-13 17:49:59 +0800

    Exler分支推进 dao改进 tk.mybatis

---
+++ movie-manager/movie-manager-dao/src/main/java/com/byx/movie/mapper/UserBaseDao.java
@@ -0,0 +1,11 @@
+package com.byx.movie.mapper;
+
+import com.byx.movie.manager.pojo.UserBase;
+import com.byx.movie.manager.pojo.UserBaseExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserBaseDao extends Mapper<UserBase> {
+
+}
